8cb63537d9496cf647f86e31f5dba42f8bb76833
Valerie Chatelet <vlrchtlt@gmail.com>
2016-06-30 16:14:44 +0200

    new web chat and index.html

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ More info about LUIS at :
 
 -----------------------------------------------------------------------------*/
 
-var builder = require('../../');
-//var builder = require('./botbuilder');
+//var builder = require('../../');
+var builder = require('./botbuilder');
 
 var dialog = new builder.LuisDialog('https://api.projectoxford.ai/luis/v1/application?id=6c459053-219e-4a87-b844-6308c71e4332&subscription-key=1fd3435757ae474ca219e29c517f5137&q=');
 var bot = new builder.TextBot();

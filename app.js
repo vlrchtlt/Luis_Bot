11f4c1486e0b5493d28b55ff47f66a5e0d07a26a
Valerie Chatelet <vlrchtlt@gmail.com>
2016-06-30 16:19:39 +0200

    added server

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ More info about LUIS at :
 -----------------------------------------------------------------------------*/
 
 //var builder = require('../../');
+
+var restify = require('restify');
 var builder = require('./botbuilder');
 
 var dialog = new builder.LuisDialog('https://api.projectoxford.ai/luis/v1/application?id=6c459053-219e-4a87-b844-6308c71e4332&subscription-key=1fd3435757ae474ca219e29c517f5137&q=');
@@ -22,5 +24,21 @@ dialog.on('None', builder.DialogAction.send("I'am not sure what you mean yet. I 
 
 
 bot.listenStdin();
+
+// Setup Restify Server
+var server = restify.createServer();
+
+// Handle Bot Framework messages
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+
+// Serve a static web page
+server.get(/.*/, restify.serveStatic({
+  'directory': '.',
+  'default': 'index.html'
+}));
+
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
 
 
